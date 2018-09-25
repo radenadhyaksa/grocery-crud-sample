f66b248cc6688153357e42f94fa31de74e0af919
Raden Adhyaksa <raksa69@gmail.com>
2018-09-25 19:53:14 +0700

    Update css to sidebar

---
+++ application/views/test.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <?php 
+foreach($css_files as $file): ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+ 
+<?php endforeach; ?>
+<?php foreach($js_files as $file): ?>
+ 
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+    <style type='text/css'>
+body
+{
+    font-family: Arial;
+    font-size: 14px;
+}
+a {
+    color: blue;
+    text-decoration: none;
+    font-size: 20px;
+}
+a:hover
+{
+    text-decoration: underline;
+}
+
+.sidenav {
+    height: 100%;
+    width: 160px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 20px;
+    color: #818181;
+    display: block;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+.main {
+    margin-left: 160px; /* Same as the width of the sidenav */
+    font-size: 28px; /* Increased text to enable scrolling */
+    padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+<body>
+<div class="main">
+<?php echo $output; ?>
+
+
+ 
+    </div>
+<!-- Beginning footer -->
+<div></div>
+<!-- End of Footer -->
+</body>
+</html>
